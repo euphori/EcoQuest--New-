cf97e2cb9c91c278bdb6a9d6c4ae7c1acb139da2
polcuenca <115073490+polcuenca@users.noreply.github.com>
2023-09-25 23:16:54 +0800

    Dialogue System

---
+++ global.gd
@@ -0,0 +1,17 @@
+extends Node
+
+var questStart = true
+
+#Questions
+var question1_complete = true
+var question2_complete = true
+var question3_complete = true
+var question4_complete= true
+var question5_complete = true
+
+#Quest Completion
+var questOne_complete = true
+var questTwo_complete = true
+var questThree_complete = true
+var questFour_complete = true
+var questFive_complete = false
