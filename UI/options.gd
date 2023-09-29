4cd35706fd7c8d53f99a61edc09b5b636bc2f37f
polcuenca <115073490+polcuenca@users.noreply.github.com>
2023-09-30 03:08:57 +0800

    option mechanics

--- UI/options.gd
+++ UI/options.gd
@@ -1,24 +1,25 @@
 extends Control
 
 
-
-		
-
-
 func _on_controller_pressed():
 	$ControllerGroup.visible = true
 	$AudioGroup.visible = false
+	$Mechanics.visible = false
 	
 func _on_audio_pressed():
 	$AudioGroup.visible = true
 	$ControllerGroup.visible = false
+	$Mechanics.visible = false
 
 
 func _on_video_pressed():
 	$AudioGroup.visible = false
 	$ControllerGroup.visible = false
+	$Mechanics.visible = false
 
 
 func _on_mechanics_pressed():
+	$Mechanics.visible = true
 	$AudioGroup.visible = false
 	$ControllerGroup.visible = false
+	
