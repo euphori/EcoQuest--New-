0bf20685c517c213f52b8c6e0f342a0983499d12
euphori <56996213+euphori@users.noreply.github.com>
2023-11-10 02:50:53 +0800

    quick fix

--- WorldScene.gd
+++ WorldScene.gd
@@ -2,6 +2,7 @@ extends Node3D
 
 @onready var manager = $CharacterManager
 @onready var scatter = $ProtonScatter
+@onready var map_label = $CanvasLayer/Label
 
 @export var intro_cam_pos : Vector3
 
@@ -21,15 +22,17 @@ func _ready():
 
 
 func play_intro():
-	$CanvasLayer/Label.visible = true
-	manager.disable_cam_control = true
-	var old_pos = manager.camera.global_position
-	manager.camera.global_position = intro_cam_pos
-	await get_tree().create_timer(2).timeout
-	var tween = get_tree().create_tween()
-	await tween.tween_property(manager.camera , "global_position" , old_pos , 5).finished
-	$CanvasLayer/Label.visible = false
-	manager.disable_cam_control = false
+	if map_label != null:
+		map_label.text = global.curr_scene_name
+		map_label.visible = true
+		manager.disable_cam_control = true
+		var old_pos = manager.camera.global_position
+		manager.camera.global_position = intro_cam_pos
+		await get_tree().create_timer(2).timeout
+		var tween = get_tree().create_tween()
+		await tween.tween_property(manager.camera , "global_position" , old_pos , 5).finished
+		map_label.visible = false
+		manager.disable_cam_control = false
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
